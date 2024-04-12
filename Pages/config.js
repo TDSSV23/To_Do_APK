3b9b57c7d82de028c796cf47620bf2ac6dcaeeee
May_Edu <mayck22.03@hotmail.com>
2024-04-12 09:15:54 -0300

    Update

--- Pages/config.js
+++ Pages/config.js
@@ -7,30 +7,27 @@ import { ThemeContext } from '../Styles/temaContext'; // Importe o ThemeContext
 import { BackHandler } from 'react-native';
 
 export default function Header() {
-  const navigation = useNavigation();
   const { theme, toggleTheme, configTextColor } = useContext(ThemeContext); // Acesse a cor do texto do botão
+  const navigation = useNavigation();
 
   return (
     <View style={[styles.container, { backgroundColor: theme === 'light' ? '#fff' : '#000' }]}>
-      <View>
-        <Image source={require('../assets/logo-app.jpeg')} style={{ width: 80, height: 80, borderRadius: 10 }} />
-        {/* <TouchableOpacity onPress={() => navigation.navigate('Config')}>
-          <Text style={[styles.texto, {color: configTextColor}]}>Apagar Contatos</Text>
-        </TouchableOpacity> */}
-        <TouchableOpacity onPress={toggleTheme}>
-          <Text style={[styles.texto, { color: configTextColor }]}>Mudar Tema</Text>
+      <View style={styles.innerContainer}>
+        <Image source={require('../assets/logo-app.jpeg')} style={styles.logo} />
+        <TouchableOpacity onPress={toggleTheme} style={styles.button}>
+          <Text style={[styles.texto, { color: theme === 'light' ? 'white' : 'black', backgroundColor: theme === 'light' ? 'black' : 'white',}]}>Mudar Tema</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => BackHandler.exitApp()}>
-          <Text style={[styles.texto, { color: configTextColor }]}>Fechar App</Text>
+        <TouchableOpacity onPress={() => BackHandler.exitApp()} style={styles.button}>
+          <Text style={[styles.texto, {color: theme === 'light' ? 'white' : 'black', backgroundColor: theme === 'light' ? 'black' : 'white'}]}>Fechar App</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Login', 'deslogar')}>
-          <Text style={[styles.texto, { color: configTextColor }]}>Logout</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Login', 'deslogar')} style={styles.button}>
+          <Text style={[styles.texto, { color: 'red', backgroundColor: theme === 'light' ? 'black' : 'white'}]}>Logout</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => Linking.openURL('mailto:support@example.com?subject=Estou com Problemas&body=Descreva o problema encontrado')}>
-          <Text style={[styles.texto, { color: configTextColor }]}>Suporte</Text>
+        <TouchableOpacity onPress={() => Linking.openURL('mailto:support@example.com?subject=Estou com Problemas&body=Descreva o problema encontrado')} style={styles.button}>
+          <Text style={[styles.texto, { color: theme === 'light' ? 'white' : 'black', backgroundColor: theme === 'light' ? 'black' : 'white' , }]}>Suporte</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Text style={[styles.texto, { color: configTextColor }]}>Voltar</Text>
+        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.button}>
+          <Text style={[styles.texto, { color: theme === 'light' ? 'white' : 'black', backgroundColor: theme === 'light' ? 'black' : 'white'}]}>Voltar</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -43,11 +40,28 @@ const styles = StyleSheet.create({
     width: '100%',
     padding: 20,
     paddingTop: 50,
-    height: '100%'
-
+    height: '100%',
+    alignItems: 'center', // Centraliza os itens horizontalmente
+  },
+  innerContainer: {
+    alignItems: 'center', // Centraliza os itens verticalmente
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    borderRadius: 10,
+    marginBottom: 20, // Adiciona uma margem abaixo da logo
+  },
+  button: {
+    marginVertical: 10, // Adiciona uma margem acima e abaixo dos botões
   },
   texto: {
-    fontSize: 20,
-    margin: 5
+    width: 120,
+    padding: 10,
+    borderRadius: 15,
+    alignItems: 'center',
+    color: 'white',
+    fontWeight: 800,
+    textAlign: 'center'
   }
 })
